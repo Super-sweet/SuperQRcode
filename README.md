95b15d914da46a217e7beb06979eef7b746ac877
347393193@qq.com <347393193@qq.com>
2016-12-22 09:11:15 +0800

    commit

--- README.md
+++ README.md
@@ -1,2 +1,12 @@
 # SuperQRcode
 二维码生成  支持logo和换色
+
+//设置图片和RGB值
+
+self.imageView1.image = [UIImage createQRCode:@"http://www.baidu.com" andSize:CGSizeMake(200, 200) andColor:[UIColor colorWithRed:0.5 green:0.5 blue:0 alpha:1] andLogoImageName:@"123"];
+//如果不生成logo 图片传入nil
+self.imageView2.image = [UIImage createQRCode:@"http://www.baidu.com" andSize:CGSizeMake(200, 200) andColor:[UIColor colorWithRed:0.5 green:0.8 blue:0 alpha:1] andLogoImageName:@"123"];
+//给图片添加阴影效果
+
+[UIImage setImageViewShadow:_imageView1];
+
--- SuperQRcode/ViewController.m
+++ SuperQRcode/ViewController.m
@@ -20,10 +20,11 @@
 - (void)viewDidLoad {
     [super viewDidLoad];
   
-    
+    //设置图片和RGB值
     self.imageView1.image = [UIImage createQRCode:@"http://www.baidu.com" andSize:CGSizeMake(200, 200) andColor:[UIColor colorWithRed:0.5 green:0.5 blue:0 alpha:1] andLogoImageName:@"123"];
-    
+    //如果不生成logo 图片传入nil
     self.imageView2.image = [UIImage createQRCode:@"http://www.baidu.com" andSize:CGSizeMake(200, 200) andColor:[UIColor colorWithRed:0.5 green:0.8 blue:0 alpha:1] andLogoImageName:@"123"];
+    //给图片添加阴影效果
     [UIImage setImageViewShadow:_imageView1];
     
 }
